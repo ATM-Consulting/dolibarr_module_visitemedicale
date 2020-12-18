441119db2fed554bc8abff1df7cb5555a0e1df2a
boudet jean pascal <68547382+atm-jpb@users.noreply.github.com>
2020-12-18 12:41:07 +0100

    Update modVisitemedicale.class.php
    remove user rights

--- core/modules/modVisitemedicale.class.php
+++ core/modules/modVisitemedicale.class.php
@@ -189,7 +189,7 @@ class modVisitemedicale extends DolibarrModules
 
 		$this->rights[$r][0] = $this->numero + $r;      // Permission id (must not be already used)
 		$this->rights[$r][1] = 'Consulter ses visites médicales';        // Permission label
-		$this->rights[$r][3] = 1;                                       // Permission by default for new user (0/1)
+		$this->rights[$r][3] = 0;                                       // Permission by default for new user (0/1)
 		$this->rights[$r][4] = 'read_own';                  // In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
 		$r++;
 		
